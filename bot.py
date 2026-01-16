9507fac545493285ab7de10232db2be20b2618ea
Fakhri177 <fakhrimalikmulyana@gmail.com>
2026-01-16 14:29:25 +0700

    Implement collage creation and user score command (M4L2 TASK 5)
    Added image collage functionality and score retrieval command.

--- bot.py
+++ bot.py
@@ -3,6 +3,11 @@ from discord.ext import commands, tasks
 from logic import DatabaseManager, hide_img
 from config import TOKEN, DATABASE
 import os
+import cv2
+import numpy as np
+import os
+from logic import *
+from math import sqrt, ceil, floor
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -67,6 +72,53 @@ async def on_interaction(interaction):
                 await interaction.response.send_message(content="Kamu sudah mendapatkan gambar!", ephemeral=True)
         else:
             await interaction.response.send_message(content="Maaf, seseorang sudah mendapatkan gambar ini.", ephemeral=True)
+
+
+def create_collage(image_paths):
+    images = []
+    for path in image_paths:
+        image = cv2.imread(path)
+        images.append(image)
+
+    num_images = len(images)
+    num_cols = floor(sqrt(num_images)) # Cari jumlah gambar secara horizontal
+    num_rows = ceil(num_images/num_cols)  # Cari jumlah gambar secara vertikal
+    # Membuat kolase kosong
+    collage = np.zeros((num_rows * images[0].shape[0], num_cols * images[0].shape[1], 3), dtype=np.uint8)
+    # Menempatkan gambar pada kolase
+    for i, image in enumerate(images):
+        row = i // num_cols
+        col = i % num_cols
+        collage[row*image.shape[0]:(row+1)*image.shape[0], col*image.shape[1]:(col+1)*image.shape[1], :] = image
+    return collage
+
+
+
+
+@bot.command()
+async def get_my_score(ctx):
+    user_id = ctx.author.id
+    m = DatabaseManager(DATABASE)
+    info = m.get_winners_img(user_id)
+    prizes = [x[0] for x in info]
+    image_paths = os.listdir('img')
+    image_paths = [f'img/{x}' if x in prizes else f'hidden_img/{x}' for x in image_paths]
+    collage = create_collage(image_paths)
+
+    output_path = f"collage_{ctx.author.id}.png"
+    cv2.imwrite(output_path, collage)
+
+    await ctx.send(
+        content="Ini kolase hadiah kamu 🎉",
+        file=discord.File(output_path)
+    )
+
+    await ctx.send(f"Kamu telah memenangkan {len(prizes)} hadiah!")
+
+
+    os.remove(output_path)
+
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}!')
